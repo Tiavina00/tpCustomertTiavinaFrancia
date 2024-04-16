d762cbeb36b1b72a24d6b6a754a4759023a38f1a
TiavinaFrancia42 <randrianarivofrancia@gmail.com>
2024-04-16 15:40:01 +0300

    Ajout de lien hyperlink pour les détails d'un customer et ajout d'un backing bean pour le customer détail

--- src/main/java/mg/itu/tpcustomertiavinafrancia/service/CustomerManager.java
+++ src/main/java/mg/itu/tpcustomertiavinafrancia/service/CustomerManager.java
@@ -33,5 +33,9 @@ public class CustomerManager {
     @Transactional
     public Customer update(Customer customer) {
       return em.merge(customer);  
-    }          
+    }   
+    
+    public Customer findById(int idCustomer) {  
+        return em.find(Customer.class, idCustomer);  
+    }
 }
